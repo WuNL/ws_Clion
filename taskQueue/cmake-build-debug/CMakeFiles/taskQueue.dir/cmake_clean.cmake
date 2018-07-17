aff8375d9249892c478eba2b7357c5a9a5a69b4a
WuNL <wunailiang@gmail.com>
2018-07-17 23:11:47 +0800

    add project smartPtr

---
+++ taskQueue/cmake-build-debug/CMakeFiles/taskQueue.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/taskQueue.dir/main.cpp.o"
+  "taskQueue.pdb"
+  "taskQueue"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/taskQueue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
