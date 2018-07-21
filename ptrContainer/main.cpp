7e1af023aea75d16dd4e4decc9a282f3f6c902cf
WuNL <wunailiang@gmail.com>
2018-07-21 19:17:32 +0800

    update ptr_vector demo

--- ptrContainer/main.cpp
+++ ptrContainer/main.cpp
@@ -1,5 +1,6 @@
 #include <boost/ptr_container/ptr_vector.hpp>
 #include <boost/shared_ptr.hpp>
+#include <memory>
 #include <vector>
 #include <cstdio>
 #include <ctime>
@@ -17,15 +18,26 @@ public:
     }
 };
 
-const int TEST_ITERATIONS = 10000000;
+const int TEST_ITERATIONS = 100000;
 
 typedef std::vector<boost::shared_ptr<TSomeData> > TVectorOfShared;
+typedef std::vector<std::unique_ptr<TSomeData> > TVectorOfUnique;
 typedef boost::ptr_vector<TSomeData> TPtrVector;
 
 int main()
 {
     clock_t start;
     clock_t end;
+
+    start = ::clock();
+    TVectorOfUnique vectorOfUnique;
+    for (int i = 0; i < TEST_ITERATIONS; ++i) {
+        std::unique_ptr<TSomeData> data(new TSomeData(i));
+        vectorOfUnique.push_back(std::move(data));
+    }
+    end = ::clock();
+    printf("Vector of unique:\n  Time executed: %u\n",
+           static_cast<uint32_t>((end - start) / (CLOCKS_PER_SEC/1000)));
 
     start = ::clock();
     TVectorOfShared vectorOfShared;
@@ -48,5 +60,8 @@ int main()
     end = ::clock();
     printf("PtrVector:\n  Time executed: %u\n",
            static_cast<uint32_t>((end - start) / (CLOCKS_PER_SEC/1000)));
+
+    TSomeData* lek;
+    lek = new TSomeData(1);
     return 0;
 }
